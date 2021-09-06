244cf7cdadea622dad530c6704f1b21c77048ae7
dmtwood <dimitri.gevers@gmail.com>
2021-09-06 11:26:28 +0200

    Belgium Account Number tests

---
+++ src/main/java/domain/AccountNumber.java
@@ -0,0 +1,41 @@
+package domain;
+
+import java.util.regex.Pattern;
+
+/*
+ * an AccountNumber is valid in Belgium when it:
+ * starts with BE,
+ * has 14 digits,
+ * first two digits > 1 & < 99
+ * ( last 12 digits, append 1114, append first two digits ) % 97 = 1
+ */
+public class AccountNumber {
+    // checks if starts with BE and has exactly 14 digits
+    // In Java regex, a \ is represented by \\
+    private static final Pattern REG_EX_BE_ACC_NR = Pattern.compile("BE\\d{14}$");
+    private final String accountNumber;
+
+    public AccountNumber(String accountNumber) {
+
+        if ( ! REG_EX_BE_ACC_NR.matcher(accountNumber).matches() )
+            throw new IllegalArgumentException("Verkeerd formaat. Begin met BE en geef dan 14 cijfers in.");
+
+        long checkNr = Long.parseLong(accountNumber.substring(2, 4));
+        if (checkNr < 2L || checkNr > 98L) {
+           throw new IllegalArgumentException("Kijk je eerste twee cijfers na. (01 en 99 kunnen niet");
+        }
+
+        // you can also use Long.valueOf(String s) to convert a string to a long
+       if( Long.parseLong(accountNumber.substring(4) + "1114" + checkNr) % 97 != 1 ) {
+           throw new IllegalArgumentException("Verkeerd formaat. Kijk alle cijfers na.");
+       }
+
+       this.accountNumber = accountNumber;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Rekeningnummer: " + accountNumber;
+    }
+}
---
+++ src/test/java/domain/AccountNumberTest.java
@@ -0,0 +1,100 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+* Checks wether an AccountNumber is valid in Belgium:
+* starts with BE,
+* has 14 digits,
+* first two digits > 1 & < 99
+* ( last 12 digits, append 1114, append first two digits ) % 97 = 1
+ */
+public class AccountNumberTest {
+
+    @Test
+    void BE72091012240116isACorrectBeAccount() {
+        new AccountNumber("BE72091012240116");
+    }
+
+    @Test
+    void notEnoughCharsThrowsException() {
+        Assertions
+                .assertThatIllegalArgumentException().isThrownBy(
+                        () -> new AccountNumber("BE0000000"));
+    }
+
+    @Test
+    void tooMuchCharsThrowsException() {
+        Assertions
+                .assertThatIllegalArgumentException().isThrownBy(
+                        () -> new AccountNumber("BE12345678901234567890")
+                );
+    }
+
+    @Test
+    void aNonBePrefixThrowsAnException() {
+        Assertions
+                .assertThatIllegalArgumentException().isThrownBy(
+                        () -> new AccountNumber("NL72091012240116")
+                );
+    }
+
+    @Test
+    void wrongCharsThrowAnException() {
+        Assertions
+                .assertThatIllegalArgumentException().isThrownBy(
+                        () -> new AccountNumber("BEL20910122401_!")
+                );
+    }
+
+    @Test
+    void tooLowCheckNrThrowsException() {
+        Assertions
+                .assertThatIllegalArgumentException().isThrownBy(
+                        () -> new AccountNumber("BE01091012240116")
+                );
+    }
+
+    @Test
+    void tooHighCheckNrThrowsException() {
+        Assertions
+                .assertThatIllegalArgumentException().isThrownBy(
+                        () -> new AccountNumber("BE99091012240116")
+                );
+    }
+
+    @Test
+    void aWrongCheckCalculationThrowsAnException() {
+        Assertions
+                .assertThatIllegalArgumentException().isThrownBy(
+                        () -> new AccountNumber("BE72091012240115")
+                );
+    }
+
+    @Test
+    void emptyThrowsAnException() {
+        Assertions
+                .assertThatIllegalArgumentException().isThrownBy(
+                        () -> new AccountNumber(" ")
+                );
+    }
+
+    @Test
+    void nullThrowsAnNullPointerException() {
+        Assertions
+                .assertThatNullPointerException().isThrownBy(
+                        () -> new AccountNumber(null)
+                );
+    }
+
+    @Test
+    void toStringShowsCorrectString() {
+        Assertions
+                .assertThat(new AccountNumber("BE72091012240116"))
+                .hasToString("Rekeningnummer: BE72091012240116");
+    }
+
+}
