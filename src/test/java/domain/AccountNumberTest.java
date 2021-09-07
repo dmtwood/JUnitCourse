53c2a6e17019599c7e0c879ebfae4fdc06421a8b
dmtwood <dimitri.gevers@gmail.com>
2021-09-07 09:46:59 +0200

    parameterized test

--- src/test/java/domain/AccountNumberTest.java
+++ src/test/java/domain/AccountNumberTest.java
@@ -3,6 +3,8 @@ package domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,10 +18,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountNumberTest {
 
 
+    // Use params when testing a feature (correct) of method multiple times (with other values)
+//    @Test
+//    void BE72091012240116isACorrectBeAccount() {
+//        new AccountNumber("BE72091012240116");
+//    }
+//
+//    @Test
+//    void BE68539007547034IsCorrect() {
+//        new AccountNumber("BE68539007547034");
+//    }
 
-    @Test
-    void BE72091012240116isACorrectBeAccount() {
-        new AccountNumber("BE72091012240116");
+    // Use params when testing a feature (correct) of method multiple times (with other values)
+    @ParameterizedTest
+    @ValueSource( strings = { "BE72091012240116" , "BE68539007547034" } )
+    void constructor_withValidNrs( String number) {
+        new AccountNumber(number);
     }
 
     @Test
